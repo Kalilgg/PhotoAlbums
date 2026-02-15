62e166b4f7c140826adaac29096b95b486abbbbd
Kalil Anthony Carvalho Davi <kalilanthony12@gmail.com>
2026-02-15 16:51:13 -0300

    fix:comentario repositorio

--- lib/data/repositories/comentario_repository.dart
+++ lib/data/repositories/comentario_repository.dart
@@ -2,5 +2,5 @@ import 'package:photo_album/domain/entities/comentario.dart';
 
 abstract class ComentarioRepository {
   Future<List<Comentario>> listar(int fotoId);
-  Future<Comentario> adicionar(int fotoId, Comentario comentario);
+  Future<Comentario> adicionar(Comentario comentario);
 }
